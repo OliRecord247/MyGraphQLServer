96aed4fcf4bacbff1e276fa2d8229b63ab3d25a6
Oliver Verdesseldonck <oliver.verdesseldonck@icapps.com>
2025-11-07 13:56:30 +0100

    Remove fixed todo in list

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -3,27 +3,29 @@ import { Todo } from './todo.entity';
 
 @Injectable()
 export class TodoService {
-  private todos: Todo[] = [
-    { id: '1', text: 'Learn Nest GraphQL', done: false },
-  ];
+  private todos: Todo[] = [];
 
   findAll(): Todo[] {
     return this.todos;
   }
+
   findOne(id: string): Todo | undefined {
     return this.todos.find((t) => t.id === id);
   }
+
   add(text: string): Todo {
     const t: Todo = { id: String(Date.now()), text, done: false };
     this.todos.push(t);
     return t;
   }
+
   toggle(id: string): Todo {
     const t = this.findOne(id);
     if (!t) throw new Error('Not found');
     t.done = !t.done;
     return t;
   }
+
   delete(id: string): boolean {
     const i = this.todos.findIndex((t) => t.id === id);
     if (i === -1) return false;
