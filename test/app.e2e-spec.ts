591b89a896f907080a4a00efa83bff55239b32bc
Oliver Verdesseldonck <oliver.verdesseldonck@icapps.com>
2025-11-07 13:47:31 +0100

    Set after all to close app

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,6 +21,10 @@ describe('AppController (e2e)', () => {
     await app.getHttpAdapter().getInstance().ready();
   });
 
+  afterAll(async () => {
+    await app.close();
+  });
+
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
